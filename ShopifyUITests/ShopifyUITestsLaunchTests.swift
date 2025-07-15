0878d3314e7655ecd094822e458fddf36630a3d6
İsmail Palalı <ismail.palali.pp@gmail.com>
2025-07-15 14:57:44 +0300

    Initial Commit

---
+++ ShopifyUITests/ShopifyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ShopifyUITestsLaunchTests.swift
+//  ShopifyUITests
+//
+//  Created by İsmail Palalı on 15.07.2025.
+//
+
+import XCTest
+
+final class ShopifyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
